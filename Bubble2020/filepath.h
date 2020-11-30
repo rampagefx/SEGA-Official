e157a4212dda414bf378c2d196aa8f218887f522
AkkoDiana <510202349@qq.com>
2020-11-30 22:23:55 +0800

    filepath update

--- Bubble2020/filepath.h
+++ Bubble2020/filepath.h
@@ -14,6 +14,16 @@ const char *map_image_path[6] = {"../../../../img/floor.png",
 const char *npc_pic_path = "../../../../img/npc.png";
 const char *button_pic_path = "../../../../img/continuebutton.png";
 #elif _WIN64
-const char *win_pic_path = "";
+const char *win_pic_path = "../img/win.png";
+const char *lose_pic_path = "../img/lose.png";
+const char *map_file_path[2] = {"../map1.txt", "../map2.txt"};
+const char *map_image_path[6] = {"../img/floor.png",
+                                 "../img/brick.png",
+                                 "../img/wall.png",
+                                 "../img/bomb.png",
+                                 "../img/out.png",
+                                "../img/explode.png"};
+const char *npc_pic_path = "../img/npc.png";
+const char *button_pic_path = "../img/continuebutton.png";
 #endif
 #endif // FILEPATH_H
