30991146c452348e69b073dea46c9b098caad3fc
XUANTONG1999 <AlexZhangmt1999@163.com>
2020-12-21 23:49:40 +0800

    Create character Slime.

---
+++ Bubble2020/slime.cpp
@@ -0,0 +1,21 @@
+#include "slime.h"
+
+Slime::Slime(int HP_in, int id_in, int role_in, int location_x_in, int location_y_in):
+    Character (HP_in, id_in, role_in, location_x_in, location_y_in)
+{
+#ifdef __APPLE__
+    pic_path = "../../../../img/pikachu.png";
+    big_pic_path = "../../../../img/pikachu_big.png";
+    discription = "恢复一点血量";
+#elif _WIN64
+    pic_path = "../img/pikachu.png";
+    big_pic_path = "../img/pikachu_big.png";
+#endif
+}
+
+void Slime::skill()
+{
+    Set_HP(Get_HP()+1);
+
+    return;
+}
